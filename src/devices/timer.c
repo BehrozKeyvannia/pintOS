023f16c05fdfdc8d63b78c634c9ed13bca88e291
Beyruuz <keyvannia.behroz@gmail.com>
2016-01-05 15:28:19 +0100

    first init; original timer.c and thread.h set

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -30,9 +30,6 @@ static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
-/* List of processes in THREAD_BLOCKED state. */
-static struct list timer_wait_list;
-
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
@@ -40,7 +37,6 @@ timer_init (void)
 {
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
-  list_init (&timer_wait_list);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
@@ -93,21 +89,11 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  struct thread *t = thread_current ();
- 
-  if (ticks <= 0)
-    return;
-
-  t->wakeup_time = timer_ticks () + ticks;
+  int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  
-  intr_disable ();
-  list_insert_ordered (&timer_wait_list, &t->timer_elem, less_wakeup, NULL);
-  intr_enable ();
-
-  /* Thread blocks itself by calling thread_block (). */
-  sema_down (&t->timer_sema);
+  while (timer_elapsed (start) < ticks) 
+    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -184,25 +170,8 @@ timer_print_stats (void)
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
-  struct thread *t;
-
   ticks++;
   thread_tick ();
-
-  enum intr_level old_level = intr_disable ();
-  while (!list_empty (&timer_wait_list))
-  {
-    t = list_entry (list_front (&timer_wait_list), struct thread, timer_elem);
-
-    /* Since the list is ordered, the first element is the earliest
-        to wake up. If it must wait, all others must wait. */
-    if (ticks < t->wakeup_time)
-      break;
-    
-    sema_up (&t->timer_sema);
-    list_pop_front (&timer_wait_list);
-  }
-  intr_set_level (old_level);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
